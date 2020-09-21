970e25343d94dad3e259a9319700eae15c0915cf
Hilmaww <hilmi.ambari27@gmail.com>
2020-09-21 20:57:09 +0800

    uploader added

--- landing/urls.py
+++ landing/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [path("", views.home, name="landing-home")]
+urlpatterns = [path("", views.home, name="landing-home"),
+                path("upload/", views.upload, name="landing-upload")
+            ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
