0c831676e39a4b26b1498f751ec42cbe8fd01414
Hilmaww <hilmi.ambari27@gmail.com>
2020-11-11 15:28:48 +0800

    trigger email

--- mail/views.py
+++ mail/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 
 from django.core.mail import send_mail
 from django.conf import settings
+
+from django.shortcuts import redirect
 # Create your views here.
 
 def email(request):
     subject = 'Thank you for registering to our site'
     message = ' it  means a world to us '
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['receiver@gmail.com',]
+    recipient_list = ['16520055@std.stei.itb.ac.id',]
     send_mail( subject, message, email_from, recipient_list )
-    return redirect('redirect to a new page')
+    return redirect('https://google.com/')
+
--- mailtest/settings.py
+++ mailtest/settings.py
@@ -100,12 +100,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 #Email Setup
-EMAIL_BACKEND = ‘django.core.mail.backends.smtp.EmailBackend’
-EMAIL_HOST = ‘mail.ganeshaicc.my.id’
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'mail.ganeshaicc.my.id'
 EMAIL_USE_SSL = True
 EMAIL_PORT = 465
-EMAIL_HOST_USER = ‘hilmifaww@ganeshaicc.my.id’
-EMAIL_HOST_PASSWORD = ‘muridhilmi27’
+EMAIL_HOST_USER = 'hilmifaww@ganeshaicc.my.id'
+EMAIL_HOST_PASSWORD = 'muridhilmi27'
 
 
 
