50aa6f73c0e9821857ece2bd54a22dc96ea32eef
Retorikal <reinardras@gmail.com>
2020-12-19 00:46:47 +0700

    Migration reset, database delete, clear cache

--- gicc2021/settings.py
+++ gicc2021/settings.py
@@ -87,9 +87,13 @@ WSGI_APPLICATION = "gicc2021.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ganeshai_db',
+        'HOST': '',
+        'PORT': '3306',
+        'USER': 'ganeshai_admin',
+        'PASSWORD': 'icc_ganesha_2021_bisa'
     }
 }
 
