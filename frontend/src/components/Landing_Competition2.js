c835e1752f26fa18c35e24c44243d7402ccb4cea
mon-git <69724010+mon-git@users.noreply.github.com>
2020-12-24 22:53:13 +0700

    Create Landing_Competition2.js

---
+++ frontend/src/components/Landing_Competition2.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Landing_Competition2 = () => {
+  return (
+    <div className="container2">
+      <div className="landing-competition2">
+        <ul>
+          <li>
+            <h3>Operations</h3>
+            <p>
+              In this sector, Paterion.id, an online class and try out platform, will give you a case of blablabla
+            </p>
+          </li>
+          <li>
+            <h3>Marketing</h3>
+            <p>
+              You will need to open your mind to create the most creative idea to solve EolaPlus case in
+              blablala
+            </p>
+          </li>
+          <li>
+            <h3>EHS</h3>
+            <p>
+              EcoPlast, an environmental startup that is eager to blablabla
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Landing_Competition;
